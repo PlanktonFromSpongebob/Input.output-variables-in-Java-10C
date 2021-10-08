c17ca5480a995a8e752c04c17e53e10842d780a5
PlanktonFromSpongebob <92122804+PlanktonFromSpongebob@users.noreply.github.com>
2021-10-08 12:19:12 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("the atom is the smallest unit of life!");
+  }
+}
+/*this is a comment*/
+//this is another way to comment lol this is so cool wowowoowowow i really like this its so cooooooooooooooool wowowo this is still a comment wow wowowowowowowowowow
+/* java variable
+you have 5 types of varibles in java 
+boolean- true/false only 2 states
+integers- Int, always numbers a;ways whole
+strings- Text, ex, "hello" always in quotation
+floats - always decimal 19.999
+char- single characters, "a", "n" write a code that gives you how many characters are in the longest word in english (insert word here)
+
+in math you say x=4 it means 4=x
+in programming x=4 means 4 is assigned to x in x i can assign anythng i want to x
+i really like this class
+
+x="patty"
+b="tomato"
+c="cheese"
+d="bread"
+e="lettuce"
+f="ketchup"
+g="mayo"
+
+so the template:type variable=value;
+*/
